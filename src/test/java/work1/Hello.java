7596cdc9f7813ebfd6640129caea2f02d310dfeb
Samet Acar <smtacar13@gmail.com>
2021-12-16 11:35:49 +0100

    added more hello feature

--- src/test/java/work1/Hello.java
+++ src/test/java/work1/Hello.java
@@ -8,6 +8,8 @@ public class Hello {
         System.out.println("Created Git Repository");
         System.out.println("New work added");
         System.out.println("Made few commit");
-
+        System.out.println("Developing hello feature");
+        System.out.println("Local Repo is Already Published");
+        System.out.println("Doing more work");
     }
 }
