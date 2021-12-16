5edf3d140918cf25455380091c80c7b3cad21ebb
Samet Acar <smtacar13@gmail.com>
2021-12-16 10:56:24 +0100

    afew work added

--- src/test/java/work1/Hello.java
+++ src/test/java/work1/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         System.out.println("Work1 started");
         System.out.println("Created Git Repository");
         System.out.println("New work added");
-
+        System.out.println("Made few commit");
 
     }
 }
